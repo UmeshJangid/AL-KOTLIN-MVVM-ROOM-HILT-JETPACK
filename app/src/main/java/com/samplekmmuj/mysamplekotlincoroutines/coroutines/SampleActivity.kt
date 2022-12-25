9fdcbdb57e89de87401b877a93e1abed9e11613a
Umesh Jangid <umesh.j@ddistechnologies.com>
2022-12-25 05:57:53 +0530

    Initial Commit

--- app/src/main/java/com/samplekmmuj/mysamplekotlincoroutines/coroutines/SampleActivity.kt
+++ app/src/main/java/com/samplekmmuj/mysamplekotlincoroutines/coroutines/SampleActivity.kt
@@ -26,17 +26,18 @@ class SampleActivity : AppCompatActivity() {
 //        }
 
         // Introduction of Suspend Functions:
-         /**
+        /**
          * A suspending function is simply a function that can be paused and resumed at a later time. They can execute a long running operation and wait for it to complete without blocking.
          * The syntax of a suspending function is similar to that of a regular function except for the addition of the suspend keyword.
          **/
-        GlobalScope.launch{
+        GlobalScope.launch {
             val networkCallAnswer = doNetworkCalling()
-            Log.d("Main-Activity",networkCallAnswer)
+            Log.d("Main-Activity", networkCallAnswer)
         }
 
         CoroutineScope(Dispatchers.Default).launch {
-            execute()
+            //execute()
+            executeJobsUnderstanding()
         }
     }
 
@@ -49,5 +50,29 @@ class SampleActivity : AppCompatActivity() {
         delay(5000)
         Log.d(TAG, Thread.currentThread().name)
         Log.d(TAG, "execute: Done.")
+    }
+
+
+    /**
+     * 1. What are Coroutines Job & Job Hierarchy?
+    2. Learn how to cancel coroutines using cancel method
+    3.Cancellation Exception Handing in Coroutines - Structured Concurrency
+     * */
+    suspend fun executeJobsUnderstanding() {
+        // Nesting Coroutines: Jobs and Cancellations
+        var parentJob = GlobalScope.launch(Dispatchers.Main) {
+            Log.d(TAG, "onCreate: Parent Job Started")
+            Log.d(TAG, "onCreate: Parent Context: $coroutineContext")
+            var childJob = CoroutineScope(Dispatchers.IO).launch {
+                Log.d(TAG, "onCreate: Child Job Started")
+                Log.d(TAG, "onCreate: Child Coroutine Context: $coroutineContext")
+                delay(6000)
+                Log.d(TAG, "onCreate: Child Job Finish")
+            }
+            delay(3000)
+            Log.d(TAG, "onCreate: Parent Job Finish")
+        }
+        parentJob.join()
+        Log.d(TAG, "onCreate: Parent Job Complete")
     }
 }
--- app/src/test/java/com/samplekmmuj/mysamplekotlincoroutines/Test.java
+++ app/src/test/java/com/samplekmmuj/mysamplekotlincoroutines/Test.java
@@ -1,2 +1,35 @@
-package com.samplekmmuj.mysamplekotlincoroutines;public class Test {
+package com.samplekmmuj.mysamplekotlincoroutines;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        integers.addAll(Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20));
+        int value =sockMerchant(9,integers);
+        System.out.println("args = " + value);
+    }
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        int counter = 0; // For pairs
+        if(n>0 && ar.size()>0){
+            for(int i=0;i<ar.size();i++){
+                for(int j=i+1;j<ar.size();j++){
+                    if(ar.get(i) == ar.get(j)){
+                        counter+=1;
+                    }else{
+                        // Nothing to do.
+                    }
+                }
+            }
+            return counter;
+        }else{
+            return 0;
+        }
+    }
 }
